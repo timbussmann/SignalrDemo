335dc2ccf3537c3ed0b69c7608d086a760685b3e
timbussmann <notthemail@youarelooking.for>
2013-12-02 22:55:48 +0100

    initial commit

---
+++ SignalrDemo/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalrDemo
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string text)
+        {
+            Clients.Caller.ShowMessage("You wrote: " + text);
+            Clients.Others.ShowMessage(Clients.Caller.Nickname + " wrote: " + text);
+        }
+    }
+}
---
+++ SignalrDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalrDemo.Startup))]
+namespace SignalrDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
